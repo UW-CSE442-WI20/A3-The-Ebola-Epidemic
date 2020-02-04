ef75c5d5b2b80dcca49c031517b36b84bc02d93f
Puja Ramanathan <puja.ramanathan@gmail.com>
2020-02-04 15:12:26 -0800

    Added dots visualization

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+const d3 = require('d3');
 
-
-console.log('hello world');
+var svgContainer = d3.select(".visual")
+                        .append("svg")
+                        .attr("width", 700)
+                        .attr("height", 200);
+var xCoord = 30;
+var yCoord = 30;
+for (var i = 0; i < 8000; i += 100) {
+    var circle = svgContainer.append("circle")
+                                .attr("cx", xCoord)
+                                .attr("cy", yCoord)
+                                .attr("r", 8)
+                                .style("fill", "gray");
+    xCoord += 30;
+    if (xCoord > 700) {
+        yCoord += 30;
+        xCoord = 30;
+    }
+}
