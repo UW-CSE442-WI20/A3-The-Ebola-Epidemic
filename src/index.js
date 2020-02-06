c64d1a6f6a4cfad3faed301200dadea3a30a4b9f
Puja Ramanathan <puja.ramanathan@gmail.com>
2020-02-05 23:16:06 -0800

    Set up for other data files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 const d3 = require('d3');
 
 var totalPeople = 0;
-d3.json("https://raw.githubusercontent.com/UW-CSE442-WI20/A3-the-ebola-epidemic/master/data/dec-2014.json").then((data) => {
-    // sum of total males and females confirmed and probable cases (cumulative)
-    totalPeople = parseInt(data.fact[15].Value) + parseInt(data.fact[16].Value);
-    console.log(totalPeople);
-    drawCircles();
-});
+
+drawNewData("https://raw.githubusercontent.com/UW-CSE442-WI20/A3-the-ebola-epidemic/master/data/dec-2014.json");
+
+drawNewData("https://raw.githubusercontent.com/UW-CSE442-WI20/A3-the-ebola-epidemic/master/data/dec-2014.json");
+
+function drawNewData(url) {
+    d3.json(url).then((data) => {
+        // sum of total males and females confirmed and probable cases (cumulative)
+        totalPeople = parseInt(data.fact[15].Value) + parseInt(data.fact[16].Value);
+        console.log(totalPeople);
+        drawCircles();
+    });
+}
 
 function drawCircles() {
     var svgContainer = d3.select(".visual")
                             .append("svg")
                             .attr("width", 700)
                             .attr("height", 200);
+
     var xCoord = 30;
     var yCoord = 30;
     for (var i = 0; i < totalPeople; i += 100) {
